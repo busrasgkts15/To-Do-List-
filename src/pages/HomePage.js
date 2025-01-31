2e471a5979206ddecc7458f3bb7e1318599eaa37
busrasgg15 <busra1gkts@gmail.com>
2025-01-31 12:22:46 +0300

    add componenets & routes

---
+++ src/pages/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { FaHome } from "react-icons/fa";
+
+const HomePage = () => {
+  return (
+    <>
+      <div>
+        <FaHome />
+        <h2>Home Page</h2>
+      </div>
+    </>
+  );
+};
+
+export default HomePage;
