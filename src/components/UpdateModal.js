ded00a3f0c960d3a2df30237c1de008dfdf13a96
busrasgg15 <busra1gkts@gmail.com>
2025-02-06 11:56:41 +0300

    Created dynamic modal

--- src/components/UpdateModal.js
+++ src/components/UpdateModal.js
@@ -15,27 +15,86 @@ import {
   Input,
 } from "reactstrap";
 
-const UpdateModal = ({ isOpen, setisOpen, data }) => {
-  const todoPost = (data) => {
+const UpdateModal = ({ isOpen, setisOpen, data, getTodoList, setupdateData }) => {
+  console.log("okan12",data)
+
+
+  const [username, setusername] = useState("") // alttaki efektin alernatifi
+  const [departmant, setDepartmant] = useState(data?.department || "")
+  const [title, setTitle] = useState(data?.title)
+
+  useEffect(() => {
+    if(data){
+      setusername(data.userName);
+    }
+
+  }, [data])
+  
+ 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("Gönderilen veri:", event);
     
-    console.log("modalda", data);
-    TodoService.TodoPost(data);
+
+    const request = {
+      userId: data.id,
+      userName: username,
+      department:departmant,
+      title: title,
+    }
+
+    if(data){
+      TodoService.TodoUpdate(request).then(res => {
+        getTodoList()
+        
+      })
+    }else{
+      TodoService.TodoPost(request).then(res => {
+        getTodoList()
+        
+      })
+    }
+
+   
+    
+    setisOpen(false); // Modal'ı kapat
+    
+
   };
+
+  const toggle = () => {
+    setupdateData(null)
+    setisOpen(false);
+
+  };
+
+  const handleChange = (e) => {
+    setusername(e.target.value);
+  };
+  const handleChangeDepartmant = (e) => {
+    setDepartmant(e.target.value);
+  };
+  const handleChangeTitle = (e) => {
+    setTitle(e.target.value);
+  };
+
 
   return (
     <>
       <Modal isOpen={isOpen}>
         <ModalHeader>Kişinin Görevini Güncelle</ModalHeader>
-        <Form onSubmit={todoPost}>
+        <Form onSubmit={handleSubmit}>
           <ModalBody>
             <Row>
               <Col md={12}>
                 <FormGroup>
-                  <Label>Name</Label>
+                  <Label for="name">Name</Label>
                   <Input
                     id="username"
                     type="text"
-                    defaultValue={data?.userName}
+                    // defaultValue={data.userName}
+                    value={username}
+                    onChange={handleChange}
                   />
                 </FormGroup>
               </Col>
@@ -45,21 +104,35 @@ const UpdateModal = ({ isOpen, setisOpen, data }) => {
                   <Input
                     id="department"
                     type="text"
-                    defaultValue={data?.department}
+                    // defaultValue={data?.department}
+                    value={departmant}
+                    onChange={handleChangeDepartmant}
                   />
                 </FormGroup>
               </Col>
               <Col md={12}>
                 <FormGroup>
                   <Label>Title</Label>
-                  <Input id="title" type="text" defaultValue={data?.title} />
+                  <Input
+                    id="title"
+                    type="text"
+                    // defaultValue={data?.title}
+                    value={title}
+                    onChange={handleChangeTitle}
+                  />
                 </FormGroup>
               </Col>
             </Row>
-            <Button color="primary" type="submit">
-              Submit
-            </Button>
-            <Button color="secondary">Cancel</Button>
+            <Row>
+              <Col md={6}>
+                <Button color="primary" type="submit">
+                  Submit
+                </Button>
+                <Button color="secondary" type="button" onClick={toggle}>
+                  Cancel
+                </Button>
+              </Col>
+            </Row>
           </ModalBody>
         </Form>
       </Modal>
